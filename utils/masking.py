5e1ca7c8545f95ad9b7885ce4c0a9ef068673857
Roman Rankl <roman.rankl@campus.lmu.de>
2026-01-19 15:15:47 +0100

    Finished Opcode masking

--- utils/masking.py
+++ utils/masking.py
@@ -6,15 +6,18 @@ def Mask_OpCodes(probability, asm_json):
         asm = json.load(f)
     for program in asm:
         for function in program['asm']:
-            for inst in function['func_instr'].values():
-                if random() < probability:
-                    pass #TODO finish
+            for key, inst in function['func_instr'].items():
+                if random.random() < probability:
+                    instList = inst.split()
+                    instList[0] = '[MASK]'
+                    function['func_instr'][key] = ' '.join(instList)
 
-    masked_asm_json = asm
-    return masked_asm_json
+    return asm
 
 def test():
     masked = Mask_OpCodes(0.3,'sample_data/curl.json')
+    with open('sample_data/masked_curl.json', 'w') as f:
+        json.dump(masked, f, indent=2)
 
 if __name__ == '__main__':
     test()
